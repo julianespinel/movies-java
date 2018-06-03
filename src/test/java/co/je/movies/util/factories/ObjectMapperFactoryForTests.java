683f8d7b9d14ea797d9c3bbfda255d1330da6119
Julian Espinel <julianespinel@gmail.com>
2018-06-03 18:44:20 -0500

    Improve ObjectMapper configuration for tests.

--- src/test/java/co/je/movies/util/factories/ObjectMapperFactoryForTests.java
+++ src/test/java/co/je/movies/util/factories/ObjectMapperFactoryForTests.java
@@ -3,14 +3,17 @@ package co.je.movies.util.factories;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 public class ObjectMapperFactoryForTests {
 
     public static ObjectMapper getConfiguredObjectMapper() {
-
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new ParameterNamesModule());
         objectMapper.registerModule(new JSR310Module());
+        objectMapper.registerModule(new Jdk8Module());
 
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
